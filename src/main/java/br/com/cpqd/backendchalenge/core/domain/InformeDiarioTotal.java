5489a3e7571e8531bf16c1f3b43b11d80940e4e5
Esiquiel Neto <ziki.neto@gamil.com>
2022-01-18 11:52:13 -0300

    Refatoração no InformeDiarioTotal

--- src/main/java/br/com/cpqd/backendchalenge/core/domain/InformeDiarioTotal.java
+++ src/main/java/br/com/cpqd/backendchalenge/core/domain/InformeDiarioTotal.java
@@ -3,31 +3,27 @@ package br.com.cpqd.backendchalenge.core.domain;
 import java.math.BigDecimal;
 
 public class InformeDiarioTotal {
-
     private String cnpj;
-    private Double captacaoDia;
+    private BigDecimal captacaoDia;
 
-    public InformeDiarioTotal() {
+    public InformeDiarioTotal(String cnpj, BigDecimal captacaoDia) {
+        this.cnpj = cnpj;
+        this.captacaoDia = captacaoDia;
     }
 
-    public InformeDiarioTotal(String cnpj, Double captacaoDia) {
-        this.cnpj = cnpj;
-        this.captacaoDia = captacaoDia.doubleValue();
+    public InformeDiarioTotal(final String csvLine) {
+
+        final String[] line = csvLine.split(";");
+
+        this.cnpj = line[0];
+        this.captacaoDia = BigDecimal.valueOf(Double.parseDouble(line[1]));
     }
 
     public String getCnpj() {
         return cnpj;
     }
 
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public Double getCaptacaoDia() {
+    public BigDecimal getCaptacaoDia() {
         return captacaoDia;
-    }
-
-    public void setCaptacaoDia(Double captacaoDia) {
-        this.captacaoDia = captacaoDia;
     }
 }
--- src/main/java/br/com/cpqd/backendchalenge/dataprovider/InformeProvider.java
+++ src/main/java/br/com/cpqd/backendchalenge/dataprovider/InformeProvider.java
@@ -8,7 +8,8 @@ import br.com.cpqd.backendchalenge.core.domain.InformeDiarioTotal;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.nio.charset.Charset;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -21,35 +22,44 @@ public class InformeProvider implements InformeBoundary {
 
     @Override
     public List<InformeDiario> getAll() {
-
-        try {
-
-            final Path path = Paths.get("src/main/resources/informes", "informe_diario.csv");
-            Reader reader = Files.newBufferedReader(path, Charset.forName("UTF-8"));
-
-            try (BufferedReader dataset = new BufferedReader(reader)) {
-                return dataset
-                        .lines()
-                        .skip(HEADER_LINE)
-                        .map(InformeDiario::new)
-                        .collect(Collectors.toList());
-            }
-        } catch (IOException ex) {
-            throw new IllegalArgumentException("Arquivo não encontrado");
+        try (BufferedReader reader = getInformeDiarioCSV()) {
+            return reader
+                    .lines()
+                    .skip(HEADER_LINE)
+                    .map(InformeDiario::new)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Erro ao realizar leitura do arquivo CSV");
         }
     }
 
 
     @Override
     public List<InformeDiarioTotal> getInformeDiarioTotalByCNPJ() {
-        return getAll()
-                .stream()
-                .map(m -> new InformeDiarioTotal(m.getCnpj(), m.getResgateDia().doubleValue()))
-                .collect(Collectors.groupingBy(InformeDiarioTotal::getCnpj, Collectors.summingDouble(InformeDiarioTotal::getCaptacaoDia)))
-                .entrySet()
-                .stream()
-                .map(e -> new InformeDiarioTotal(e.getKey(), e.getValue()))
-                .collect(Collectors.toList());
+        try (BufferedReader reader = getInformeDiarioCSV()) {
+            return reader
+                    .lines()
+                    .skip(HEADER_LINE)
+                    .map(InformeDiarioTotal::new)
+                    .collect(Collectors.groupingBy(InformeDiarioTotal::getCnpj, Collectors.reducing(BigDecimal.ZERO, InformeDiarioTotal::getCaptacaoDia, BigDecimal::add)))
+                    .entrySet()
+                    .stream()
+                    .map(e -> new InformeDiarioTotal(e.getKey(), e.getValue()))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Erro ao realizar leitura do arquivo CSV");
+        }
+    }
+
+    private BufferedReader getInformeDiarioCSV() {
+        try {
+            final Path path = Paths.get("src/main/resources/informes", "informe_diario.csv");
+            Reader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+
+            return new BufferedReader(reader);
+        } catch (IOException ex) {
+            throw new IllegalArgumentException("Arquivo não encontrado");
+        }
     }
 
 }
